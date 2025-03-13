c585bbeefcc09eee901128cdd6ce683989b4a369
jsw0478 <jswsarang@gmail.com>
2025-03-13 16:52:23 +0900

    modified:   Crazymode.js

--- Crazymode.js
+++ Crazymode.js
@@ -1,6 +1,6 @@
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
-const img3 = document.getElementById('img3');
+const img3 = document.getElementById('img3'); 
 const img4 = document.getElementById('img4');
 const imgs = [img1, img2, img3, img4];
 
